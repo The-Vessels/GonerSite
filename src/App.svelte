229030a576069605593d05a844c936eeac91e542
Anon Ymous <ancientstraits@protonmail.com>
2026-01-09 16:05:11 -0500

    added SoulButton and text shadow

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
   import Header from './lib/Header.svelte'
+    import SoulButton from './lib/SoulButton.svelte';
 </script>
 
 <main>
-  <Header></Header>
+  <Header />
+
+  <h1 class="bold">The best way to make Deltarune fangames in Godot!</h1>
+
+  <SoulButton>Determination.</SoulButton>
 </main>
 
 <style>
---
+++ src/lib/SoulButton.svelte
@@ -0,0 +1,25 @@
+<script>
+    const { children } = $props();
+</script>
+
+<button>
+    <img src="src/assets/soul.png" alt="red human soul" />
+    {@render children()}
+</button>
+
+<style>
+    button {
+        color: white;
+        background-color: black;
+        border-style: none;
+
+        font-family: "8bitOperatorPlus-Bold", "8bitOperatorPlus-Regular", "monospace";
+        /* text-shadow: 1px 1px #000080; */
+    }
+    button img {
+        image-rendering: pixelated;
+    }
+    button:hover {
+        color: yellow;
+    }
+</style>
